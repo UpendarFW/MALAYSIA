a8207bee35a327d3efb9bfbac1d7907052c44283
upend <upend@UPENDAR1998>
2023-05-17 08:45:24 +0530

    commit 4

--- src/test/java/Practice_Only/TestNG.java
+++ src/test/java/Practice_Only/TestNG.java
@@ -1,6 +1,6 @@
 package Practice_Only;
 
-import org.testng.Assert;
+
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -10,7 +10,7 @@ import GenericUtilities.BaseClass_Utility;
 
 
  @Listeners(GenericUtilities.ListnersImplimentation.class)
-public class TestNG_Test1 extends BaseClass_Utility {
+public class TestNG extends BaseClass_Utility {
 
 	
 	 @Test(groups = "Regression",retryAnalyzer = GenericUtilities.IRetryAnalizer.class)
@@ -24,7 +24,7 @@ public class TestNG_Test1 extends BaseClass_Utility {
 	 
 	 public void TC_002_Login1(){
 	
-		 Assert.fail();
+		
 	 
 	 
 			
